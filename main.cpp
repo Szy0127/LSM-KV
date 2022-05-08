64e36ec45f94178f9c3504d3fb1b9c11296c136b
Szy0127 <1799853523@qq.com>
2022-05-08 19:57:29 +0800

    test compaction
    Signed-off-by: Szy0127 <1799853523@qq.com>

--- main.cpp
+++ main.cpp
@@ -33,43 +33,43 @@ void test_base(int length,std::ofstream &f){
     //先插入 越到后面越慢  但是统计平均时间
     auto t1 = std::chrono::high_resolution_clock::now();
     for(int i = 0 ;i < length ;i++){
-        kvStore.put(test_data[i], std::string(1000,'s'));
+        kvStore.put(test_data[i], std::string(5000,'s'));
     }
     auto t2 = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
     f<<"put latency:"<<(double)duration/length<<std::endl;
 
-    //查找 先找所有的key 然后找不存在的key
-    t1 = std::chrono::high_resolution_clock::now();
-    for(int i = 0 ;i < length ;i++){
-        kvStore.get(test_data[i]);
-    }
-    //模拟一些key不存在的情况
-    for(int i = 0 ;i < length/5 ;i++){
-        kvStore.get(getRandKey());
-    }
-    t2 = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
-    f<<"get latency:"<<(double)duration/length/1.2<<std::endl;
-
-    // 删除 先删一些不存在的  然后全删了
-    for(int i = 0 ;i < length/5 ;i++){
-        kvStore.del(getRandKey());
-    }
-    t1 = std::chrono::high_resolution_clock::now();
-    for(int i = 0 ;i < length ;i++){
-        kvStore.del(test_data[i]);
-    }
-
-    t2 = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
-    f<<"del latency:"<<(double)duration/length/1.2<<std::endl;
+//    //查找 先找所有的key 然后找不存在的key
+//    t1 = std::chrono::high_resolution_clock::now();
+//    for(int i = 0 ;i < length ;i++){
+//        kvStore.get(test_data[i]);
+//    }
+//    //模拟一些key不存在的情况
+//    for(int i = 0 ;i < length/5 ;i++){
+//        kvStore.get(getRandKey());
+//    }
+//    t2 = std::chrono::high_resolution_clock::now();
+//    duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+//    f<<"get latency:"<<(double)duration/length/1.2<<std::endl;
+//
+//    // 删除 先删一些不存在的  然后全删了
+//    for(int i = 0 ;i < length/5 ;i++){
+//        kvStore.del(getRandKey());
+//    }
+//    t1 = std::chrono::high_resolution_clock::now();
+//    for(int i = 0 ;i < length ;i++){
+//        kvStore.del(test_data[i]);
+//    }
+//
+//    t2 = std::chrono::high_resolution_clock::now();
+//    duration = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+//    f<<"del latency:"<<(double)duration/length/1.2<<std::endl;
 
 }
 int main() {
-//    std::ofstream f("../test_res/test_base_small.txt");
-    std::ofstream f("../test_res/test_base_big.txt");
-//    std::vector<int> test_length_small{1000,1500,2000,2500,3000,3500,4000,4500,5000,8000,10000};
+    std::ofstream f("../test_res/test_compaction.txt");
+//    std::ofstream f("../test_res/test_base_big.txt");
+//    std::vector<int> test_length_small{1000,1500,2000,2500,3000,3500,4000,4500,5000,5500,6000,6500,7000,7500,8000,8500,9000,9500,10000};
     std::vector<int> test_length_big{5000,10000,20000,50000,80000,100000};
     for(auto i:test_length_big){
         test_base(i,f);
